2ebfd0b40e1ea0663ab96b88cd7521435cfc1867
lukareeckate <62818427+lukareeckate@users.noreply.github.com>
2020-05-31 01:15:53 +0300

    hello world and multiline

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #this is a hello world application
 print('hello world! \n')
-print("hello world!")
+print("hello world2!")
 #multiline
 print('''this is multi line hello world
 today is a very sunny''')
